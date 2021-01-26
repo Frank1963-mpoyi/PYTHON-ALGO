a4461080a7d38f06016a0cbf76b1d1cef22a5d58
Mpoyi-Tshibuyi <mpoyitshibuyi63@gmail.com>
2021-01-26 18:20:52 +0200

    add exercise

---
+++ algo1.py
@@ -0,0 +1,34 @@
+'''
+EXERCISE 
+
+Exercise 2:
+---------
+
+write a program that uses input to prompt a user for their name and then welcomes them
+Enter your name: Frank
+Hello Frank
+
+Exercise 3:
+----------
+write a program to prompt the user for hours and rate per hour to 
+computer gross pay.
+
+Enter Hours: 35
+Enter Rate: 2.75
+Pay: 96.25
+
+we won't worry about making sure our pay has exactly two digits after the decimal
+place for now. If you want, you can play with the built-in Python round function
+to properly round the resulting pay to two decimal places.
+
+Exercise 4: 
+----------
+Assume  that we execute the following assignment statements:
+
+width = 17
+height = 12.0
+
+For each of the following expressions. write the value of the expression and the type 
+(of the value of the expression).
+
+'''
